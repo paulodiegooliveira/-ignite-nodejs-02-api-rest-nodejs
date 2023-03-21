afb2513011c085c1d5bb0351a5ca172397106b7d
Diego Oliveira <paulodiego.oliveira@gmail.com>
2023-03-21 11:50:53 -0300

    Categorizando os testes 03:05

--- test/exemple.spec.ts
+++ test/exemple.spec.ts
@@ -1,22 +1,24 @@
-import { afterAll, beforeAll, test } from 'vitest'
+import { it, afterAll, beforeAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-beforeAll(async () => {
-  await app.ready()
-})
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
-afterAll(async () => {
-  await app.close()
-})
+  afterAll(async () => {
+    await app.close()
+  })
 
-test('O usuário consegue criar uma nova transação', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201)
+  it('shoud be possible create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 })
